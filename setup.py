fdf68c774244c86d061bb095fdbddb943c4c653d
Andrew Walker <awalkerdev@gmail.com>
2015-11-30 00:19:06 +0000

    Make version compliant with pip

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ with open('requirements.txt') as f:
 setup(
   name = 'heatmappy',
   packages = ['heatmappy'],
-  version = '0.1',
+  version = '0.1.0',
   description = 'Draw image heatmaps in python',
   author = 'Lumen Research',
   author_email = 'development@lumen-research.com',
   url = 'https://github.com/LumenResearch/heatmappy',
-  download_url = 'https://github.com/LumenResearch/heatmappy/tarball/0.1',
+  download_url = 'https://github.com/LumenResearch/heatmappy/tarball/0.1.1',
   keywords = ['image', 'heatmap', 'heat map'],
   install_requires=required,
   classifiers = [
