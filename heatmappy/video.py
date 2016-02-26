dbb63c7f90edee71fa7c7501a07e24e79911e3c4
axwalker <awalkerdev@gmail.com>
2016-02-26 11:30:42 +0000

    Can draw video heatmap on static image

--- heatmappy/video.py
+++ heatmappy/video.py
@@ -4,6 +4,7 @@ import random
 
 from moviepy.editor import *
 import numpy as np
+from PIL import Image
 
 from heatmappy import Heatmapper
 
@@ -24,6 +25,23 @@ class VideoHeatmapper:
     def heatmap_on_video_path(self, video_path, points, heat_fps=15):
         base = VideoFileClip(video_path)
         return self.heatmap_on_video(base, points, heat_fps)
+
+    def heatmap_on_image(self, base_img, points, heat_fps=15, duration_s=None):
+        base_img = np.array(base_img)
+        points = list(points)
+        if not duration_s:
+            duration_s = max(t for x, y, t in points) / 1000
+        base_video = ImageClip(base_img).set_duration(duration_s)
+
+        return self.heatmap_on_video(base_video, points, heat_fps=heat_fps)
+
+    def heatmap_on_image_path(self, base_img_path, points, heat_fps=15, duration_s=None):
+        base_img = Image.open(base_img_path)
+        return self.heatmap_on_image(
+            base_img, points,
+            heat_fps=heat_fps,
+            duration_s=duration_s
+        )
 
     @staticmethod
     def _frame_points(pts, fps):
@@ -51,21 +69,22 @@ def _example_random_points():
     def rand_point(max_x, max_y, max_t):
         return random.randint(0, max_x), random.randint(0, max_y), random.randint(0, max_t)
 
-    return (rand_point(720, 480, 4000) for _ in range(15000))
+    return (rand_point(720, 480, 5000) for _ in range(15000))
 
 
 def main():
-    example_vid = os.path.join('assets', 'SampleVideo_720x480_1mb.mp4')
+    example_base_img = os.path.join('assets', 'cat.jpg')
 
     img_heatmapper = Heatmapper(colours='default', point_strength=0.6)
     video_heatmapper = VideoHeatmapper(img_heatmapper)
 
-    heatmap_video = video_heatmapper.heatmap_on_video_path(
-        video_path=example_vid,
-        points=_example_random_points()
+    heatmap_video = video_heatmapper.heatmap_on_image_path(
+        base_img_path=example_base_img,
+        points=_example_random_points(),
+        duration_s=10
     )
 
-    heatmap_video.write_videofile('out.mp4', bitrate="5000k", fps=24)
+    heatmap_video.write_videofile('out_on_image.mp4', bitrate="5000k", fps=24)
 
 if __name__ == '__main__':
     main()
